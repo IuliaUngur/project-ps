69891b611d9bf118970002933654afbafe7ba493
Dan Patiu <iuli_ungur@yahoo.com>
2016-04-09 21:48:00 +0300

    SQL database

--- Assignment 1/desk_bank .sql
+++ Assignment 1/desk_bank .sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2016 at 11:29 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: Apr 09, 2016 at 08:44 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 5.5.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `desk_bank`
@@ -26,14 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `clients`
 --
 
-CREATE TABLE IF NOT EXISTS `clients` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clients` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `cnp` int(11) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `cnp` (`cnp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `clients`
@@ -57,15 +55,13 @@ INSERT INTO `clients` (`id`, `name`, `cnp`, `address`) VALUES
 -- Table structure for table `client_accounts`
 --
 
-CREATE TABLE IF NOT EXISTS `client_accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `client_accounts` (
+  `id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
   `money_amount` int(11) NOT NULL,
   `created_on` datetime NOT NULL,
-  `owner_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `owner_id` (`owner_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+  `owner_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `client_accounts`
@@ -81,22 +77,20 @@ INSERT INTO `client_accounts` (`id`, `type`, `money_amount`, `created_on`, `owne
 -- Table structure for table `employees`
 --
 
-CREATE TABLE IF NOT EXISTS `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employees` (
+  `id` int(11) NOT NULL,
   `user_name` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `type` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `type` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employees`
 --
 
 INSERT INTO `employees` (`id`, `user_name`, `password`, `type`) VALUES
-(1, 'iuli', 'pwd', 1),
-(3, 'test', 'test', 1);
+(3, 'admin', 'admin', 0),
+(6, 'iuli', 'pwd2', 1);
 
 -- --------------------------------------------------------
 
@@ -104,15 +98,79 @@ INSERT INTO `employees` (`id`, `user_name`, `password`, `type`) VALUES
 -- Table structure for table `employee_activities`
 --
 
-CREATE TABLE IF NOT EXISTS `employee_activities` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `employee_activities` (
+  `id` int(11) NOT NULL,
   `type` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` datetime NOT NULL,
   `employee_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employee_activities`
+--
+
+INSERT INTO `employee_activities` (`id`, `type`, `date`, `employee_id`, `description`) VALUES
+(1, 0, '2016-04-13 00:00:00', 6, 'Some employee description'),
+(2, 14, '2016-04-09 19:24:26', 3, 'Added employee with user name: testt ( password: testt, type: 1)'),
+(3, 13, '2016-04-09 19:24:39', 3, 'Deleted employee with user name: testt'),
+(4, 12, '2016-04-09 19:24:48', 3, 'Updated employee user name: iuli ( password: pwd2, type: 1)');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cnp` (`cnp`);
+
+--
+-- Indexes for table `client_accounts`
+--
+ALTER TABLE `client_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `owner_id` (`owner_id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
+-- Indexes for table `employee_activities`
+--
+ALTER TABLE `employee_activities`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `client_accounts`
+--
+ALTER TABLE `client_accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `employee_activities`
+--
+ALTER TABLE `employee_activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
